7e309f29c05441c9be36af379ee5229947fcff14
GamingasCZ <gamingforyou875@gmail.com>
2024-10-10 09:06:27 +0200

    added more tests

--- src/scripts/pokemon.test.ts
+++ src/scripts/pokemon.test.ts
@@ -1,20 +1,28 @@
 import { getPokemon, POKEMON_COUNT } from "./pokemon";
 import { describe, expect, test } from "vitest";
-
+import { readdirSync } from "fs";
 
 const base = import.meta.env.BASE_URL
 
-describe("Loading Pokemon", async () => {
+describe.concurrent("Loading Pokemon", async () => {
     let pokemon = await getPokemon()
+    let portraitsDir = readdirSync("public/portraits")
+    let footprintsDir = readdirSync("public/footprints")
 
-    test("JSON structure", () => {
+    test("JSON structure, ensure Pokemon has a portrait and a footprint", () => {
         pokemon.forEach(pk => {
             expect(pk).toHaveProperty("name")
             expect(pk).toHaveProperty("id")
+            expect(portraitsDir).toContain(`${pk.id}.png`)
+            expect(footprintsDir).toContain(`${pk.id}.png`)
             expect(pk).toHaveProperty("hint")
         })
     })
 
     test("JSON length", () => expect(pokemon.length).toBe(POKEMON_COUNT))
 
+    test("Portraits length", () => expect(portraitsDir).toHaveLength(POKEMON_COUNT))
+    test("Footprints length", () => expect(footprintsDir).toHaveLength(POKEMON_COUNT))
+
 })
+
